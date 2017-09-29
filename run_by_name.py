d9a1925d508db17d8b6f986fe044223ac9378b5f
Yanyu Liang <yanyul@uchicago.edu>
2017-09-29 18:19:27 -0500

    added function

---
+++ run_by_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import argparse
+parser = argparse.ArgumentParser(prog='run_by_name.py', description='''
+    This function takes a file name and a cmd line and replace \{\} in
+    the cmd line and run it
+''')
+parser.add_argument('--cmd', help = '''
+    The command wanted. E.g. `cat \{\} | grep apple > \{\}.temp`
+''')
+parser.add_argument('name')
+args = parser.parse_args()
+
+import os
+import re
+
+cmd = re.sub('{}', args.name, args.cmd)
+print(cmd)
